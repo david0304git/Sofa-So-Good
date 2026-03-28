2533465b6c4e64b0e7f07fad3e8c36bef565d5e0
david0304git <140947445+david0304git@users.noreply.github.com>
2026-03-28 12:34:47 +0800

    Add button

--- app/src/main/java/com/speed/sofasogood/MainActivity.java
+++ app/src/main/java/com/speed/sofasogood/MainActivity.java
@@ -1,6 +1,10 @@
 package com.speed.sofasogood;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        setupButtonAnimation(findViewById(R.id.btnStart));
+        setupButtonAnimation(findViewById(R.id.btnCredits));
+        setupButtonAnimation(findViewById(R.id.btnSettings));
+        setupButtonAnimation(findViewById(R.id.btnQuit));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupButtonAnimation(View button) {
+        button.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_press));
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.button_release));
+                    break;
+            }
+            return false;
         });
     }
 }
